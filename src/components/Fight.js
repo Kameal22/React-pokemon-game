fee9d20e38fb0773bb916af7ceb929595db567e6
kamil <kami.czyzewski96@gmail.com>
2021-12-06 21:23:11 +0100

    Fight component in progress

--- src/components/Fight.js
+++ src/components/Fight.js
@@ -1,11 +1,20 @@
 import "../styles/Fight.css";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { CharacterContext } from "../contexts/playerContexts/CharacterContexts";
+import { PokemonListContext } from "../contexts/pokemonContexts/PokemonListContext";
+import { OwnedPokemonContext } from "../contexts/pokemonContexts/OwnedPokemonContext";
+import { CurrentPokemonContext } from "../contexts/pokemonContexts/CurrentPokemonContext";
 
 function Fight() {
   const { level, levelUpFunc, exp, expUpFunc, requiredExp, encounters } =
     useContext(CharacterContext);
+  const { pokemonList } = useContext(PokemonListContext);
+  const { ownedPokemon } = useContext(OwnedPokemonContext);
+  const { currentPokemon, changePokemon } = useContext(CurrentPokemonContext);
+
+  const [enemy, setEnemy] = useState([]);
+  const [fightStart, setFightStart] = useState(false);
 
   const levelUp = (level) => {
     return levelUpFunc(level);
@@ -15,14 +24,40 @@ function Fight() {
     return expUpFunc(exp);
   };
 
-  return (
-    <div className="pokedexDiv">
-      <div className="navLinksScd">
-        <Link to="/">Home</Link>
+  const start = () => {
+    setFightStart(!fightStart);
+    const randInt = Math.floor(Math.random() * pokemonList.length);
+
+    const enemy = ownedPokemon[randInt];
+    setEnemy(enemy);
+  };
+
+  if (!fightStart) {
+    return (
+      <div className="fightDiv">
+        <div className="navLinksScd">
+          <Link to="/">Home</Link>
+        </div>
+        <h2 className="fightHeading">Fight</h2>
+        <div className="fightSelectingDiv">
+          <h4 onClick={start} style={{ cursor: "pointer" }}>
+            Encounter
+          </h4>
+        </div>
       </div>
-      <h2 className="fightHeading">Fight</h2>
-      <p>TEST</p>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="fightDiv">
+        <h2 className="fightHeading">Fight</h2>
+        <div className="startedFightDiv">
+          <div className="user">
+            <p>{currentPokemon.name}</p>
+            <img src={currentPokemon.img}></img>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default Fight;
