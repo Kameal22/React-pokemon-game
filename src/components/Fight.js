3819f8d9a219d9d0501580f260f84b1c9192bc6a
Kamil Czyżewski <kamil.czyzewski96@gmail.com>
2021-12-07 15:46:03 +0100

    Fight logic improvements

--- src/components/Fight.js
+++ src/components/Fight.js
@@ -10,11 +10,11 @@ function Fight() {
   const { level, levelUpFunc, exp, expUpFunc, requiredExp, encounters } =
     useContext(CharacterContext);
   const { pokemonList } = useContext(PokemonListContext);
-  const { ownedPokemon } = useContext(OwnedPokemonContext);
   const { currentPokemon, changePokemon } = useContext(CurrentPokemonContext);
 
   const [enemy, setEnemy] = useState([]);
   const [fightStart, setFightStart] = useState(false);
+  const [playersTurn, setPlayersTurn] = useState(false);
 
   const levelUp = (level) => {
     return levelUpFunc(level);
@@ -24,12 +24,20 @@ function Fight() {
     return expUpFunc(exp);
   };
 
-  const start = () => {
-    setFightStart(!fightStart);
+  const showEnemy = () => {
     const randInt = Math.floor(Math.random() * pokemonList.length);
 
-    const enemy = ownedPokemon[randInt];
+    const enemy = pokemonList[randInt];
     setEnemy(enemy);
+  };
+
+  const start = () => {
+    setFightStart(!fightStart);
+    showEnemy();
+  };
+
+  const flee = () => {
+    setFightStart(!fightStart);
   };
 
   if (!fightStart) {
@@ -55,7 +63,14 @@ function Fight() {
             <p>{currentPokemon.name}</p>
             <img src={currentPokemon.img}></img>
           </div>
+          <div className="enemy">
+            <p>{enemy.name}</p>
+            <img src={enemy.img}></img>
+          </div>
         </div>
+        <button className="fleeBtn" onClick={flee}>
+          Flee
+        </button>
       </div>
     );
   }
